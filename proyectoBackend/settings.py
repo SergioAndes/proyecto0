1615c6be9ffc75ac844e5875e7f9947993487b70
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2020-01-30 09:56:52 -0500

    Update settings.py

--- proyectoBackend/settings.py
+++ proyectoBackend/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'proyectoBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': '172.24.42.22',
+        'PORT': '',
     }
 }
 
